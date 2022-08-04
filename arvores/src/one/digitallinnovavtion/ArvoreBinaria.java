b687fc8aee673d642642b4a0f6703ad5dbd6b899
Davy Lucas <61299543+KURAMA2012@users.noreply.github.com>
2022-08-04 14:20:00 -0300

    (Árvore binária)-Implementando método de inserir

---
+++ arvores/src/one/digitallinnovavtion/ArvoreBinaria.java
@@ -0,0 +1,28 @@
+package one.digitallinnovavtion;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+	
+	private BinNo<T> raiz;
+	
+	public ArvoreBinaria() {
+		this.raiz = null;
+	}
+	
+	public void inserir(T conteudo) {
+		BinNo<T> novoNo = new BinNo<>(conteudo);
+		raiz = inserir(raiz, novoNo);
+	}
+	
+	private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){	
+		if(atual == null) { 
+			return novoNo;
+		}else if(novoNo.getConteudo().compareTo(atual.getConteudo()) < 0 ) {
+			atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+		}else{
+			atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+		}
+		return  atual;
+			
+	}
+
+}
