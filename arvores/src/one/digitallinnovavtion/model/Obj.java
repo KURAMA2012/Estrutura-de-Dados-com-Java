cdc705081c1fd19c323a95f496f472add9944366
Davy Lucas <61299543+KURAMA2012@users.noreply.github.com>
2022-08-09 16:55:40 -0300

    (Árvore binária)-Implementação método de remover/finalização da classe

---
+++ arvores/src/one/digitallinnovavtion/model/Obj.java
@@ -0,0 +1,48 @@
+package one.digitallinnovavtion.model;
+
+import java.util.Objects;
+
+public class Obj extends ObjArvore<Obj> {
+
+	Integer meuValor;
+
+	public Obj(Integer meuValor) {
+		this.meuValor = meuValor;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Obj other = (Obj) obj;
+		return Objects.equals(meuValor, other.meuValor);
+	}
+
+	
+	@Override
+	public int compareTo(Obj outro) {
+		int i = 0;
+
+		if (this.meuValor > outro.meuValor) {
+			i = 1;
+		} else if (this.meuValor < outro.meuValor) {
+			i = -1;
+		}
+		return i;
+	}
+
+	@Override
+	public int hasCode() {
+		// TODO Auto-generated method stub
+		return Objects.hash(meuValor);
+	}
+	
+	@Override
+	public String toString() {
+		return meuValor.toString();
+	}
+}
---
+++ arvores/src/one/digitallinnovavtion/model/ObjArvore.java
@@ -0,0 +1,9 @@
+package one.digitallinnovavtion.model;
+
+public abstract class ObjArvore<T> implements Comparable<T> {
+	
+	public abstract boolean equals(Object o);
+	public abstract int hasCode();
+	public abstract int compareTo(T outro);
+	public abstract String toString();
+}
